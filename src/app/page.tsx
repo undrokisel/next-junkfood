26b3efc493d99e482d46539558f4ee5cff8c4f24
@undrokisel <undrokisel@gmail.com>
2024-08-20 19:15:03 +0500

    added title component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { Header } from '../components/shared/header';
+import { Title } from '../components/shared/title';
+import { Container } from '../components/shared/container';
 
 export default function Home() {
   return (
   <>
-    <h1>Home</h1>
-    <Button
-    size="lg"
-    variant="outline"
-    >Купить</Button>
+    <Container className="mt-10">
+      <Title size="lg" text="Весь ассортимент" />
+    </Container>
   </>
   );
 }
