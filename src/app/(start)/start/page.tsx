17a88efa8ca6a085885d769da3976e49943e854d
kiselandrey <undrokisel@gmail.com>
2025-05-29 16:55:38 +0500

    fix(start): adaptive

--- src/app/(start)/start/page.tsx
+++ src/app/(start)/start/page.tsx
@@ -1,44 +1,65 @@
+'use client';
+
 import { Button } from '@/components/ui';
 import Link from 'next/link';
 import { Title } from '../../../components/shared/title';
 import { Container } from '../../../components/shared/container';
+import { FadeVisible } from '../../../components/shared/animation/FadeVisible';
 
 function Start() {
   return (
     <Container className=' flex min-h-screen justify-center text-lg '>
-      <div className='bg-green-100 rounded-3xl p-10 m-10 min-h-[60vh] flex flex-col text-center gap-4 flex-grow'>
-        <Title
-          size='lg'
-          text='Веб-приложение для сети ресторанов быстрого питания'
-          className='ml-6'
-        />
-        <Title size='lg' text='&ldquo;Твоя Шаурма&ldquo;' className='ml-6' />
+      <div className='bg-green-100 rounded-3xl p-4 sm:p-10 m-4 sm:m-10 min-h-[60vh] flex flex-col text-center gap-2 sm:gap-4 flex-grow'>
+        <FadeVisible>
+          <Title
+            size='lg'
+            text='Веб-приложение для сети ресторанов быстрого питания'
+            className='sm:ml-6 text-lg sm:text-3xl'
+          />
+        </FadeVisible>
+        <FadeVisible>
+          <Title
+            size='lg'
+            text='&ldquo;Твоя Шаурма&ldquo;'
+            className='text-xl sm:text-3xl sm:ml-6'
+          />
+        </FadeVisible>
 
-        <div className='mt-20 flex-grow content-center'>
-          <div className='mb-10'>
-            <p className='text-[28px] mb-8'>Уважаемый пользователь!</p>
-            <p className='text-[28px]'>Представляю Вашему вниманию</p>
-            <p className='text-[28px]'>
-              веб-приложение для сети ресторанов быстрого питания
+        <div className='mt-4 sm:mt-20 flex-grow content-center'>
+          <FadeVisible>
+            <div className='mb-6 sm:mb-10'>
+              <p className='sm:text-[28px] mb-4 sm:mb-8'>
+                <b>Уважаемый пользователь!</b>
+              </p>
+              <p className='sm:text-[28px]'>Представляю Вашему вниманию</p>
+              <p className='sm:text-[28px]'>
+                веб-приложение для сети ресторанов быстрого питания
+              </p>
+              <p className='sm:text-[28px]'>
+                <b>&ldquo;Твоя Шаурма&ldquo;</b>
+              </p>
+            </div>
+          </FadeVisible>
+          <FadeVisible>
+            <Button className='sm:mt-8 sm:p-12'>
+              <Link href='/' className='text-lg sm:text-[32px]'>
+                На главную страницу
+              </Link>
+            </Button>
+          </FadeVisible>
+        </div>
+
+        <FadeVisible>
+          <div className='mt-4 sm:mt-7 text-center text-[16px] sm:text-lg'>
+            <p>Разработчик: студент группы 2-ИС Кисель Андрей Владимирович</p>
+            <p>
+              Специальность: 09.02.07 &ldquo;Информационные системы и
+              программирование&ldquo;
             </p>
-            <p className='text-[28px]'>&ldquo;Твоя Шаурма&ldquo;</p>
+            <p>ГБПОУ Пермский краевой колледж &ldquo;Оникс&ldquo;</p>
+            <p className='mt-4 sm:mt-8'>Пермь 2025</p>
           </div>
-          <Button className='mt-8 p-12'>
-            <Link href='/' className='text-[32px]'>
-              Переход на главную страницу сайта
-            </Link>
-          </Button>
-        </div>
-
-        <div className='mt-7 text-center'>
-          <p>Разработчик: студент группы 2-ИС Кисель Андрей Владимирович</p>
-          <p>
-            Специальность: 09.02.07 &ldquo;Информационные системы и
-            программирование&ldquo;
-          </p>
-          <p>ГБПОУ Пермский краевой колледж &ldquo;Оникс&ldquo;</p>
-          <p className='mt-8'>Пермь 2025</p>
-        </div>
+        </FadeVisible>
       </div>
     </Container>
   );
